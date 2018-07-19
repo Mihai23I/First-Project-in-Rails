e7305d07d49a073a07f1f7ddbb69b92a8abb4b41
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-19 11:18:04 +0300

    page_get test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,33 +1,43 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup; end
+
   test "should get home" do
-    get static_pages_home_url
+    page_get 'home'
     assert_response :success
   end
 
   test "should get help" do
-    get static_pages_help_url
+    page_get 'help'
     assert_response :success
   end
 
   test "should get about" do
-    get static_pages_about_url
+    page_get 'about'
     assert_response :success
   end
 
   test "is home title right" do
-    get static_pages_home_url
+    page_get 'home'
     assert_select 'title', 'Home'
   end
 
   test "is help title right" do
-    get static_pages_help_url
+    page_get 'help'
     assert_select 'title', 'Help'
   end
 
   test "is about title right" do
-    get static_pages_about_url
+    page_get 'about'
     assert_select 'title', 'About'
+  end
+
+  def page_get(page)
+    operations = {}
+    operations['about'] = -> { get static_pages_about_url }
+    operations['help'] = -> { get static_pages_help_url }
+    operations['home'] = -> { get static_pages_home_url }
+    operations[page].call
   end
 end
