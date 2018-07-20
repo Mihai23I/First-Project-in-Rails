8606891a6a63dc2e846b53e17985f807d962d340
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-20 14:43:31 +0300

    update till 6.2.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
+
+  # Name Validations
   validates :name, presence: true
+  validates :name, length: { maximum: 50,
+                             minimum: 3 }
+  VALID_NAME_REGEX = /\A[a-z]+\z/i
+  validates :name, format: { with: VALID_NAME_REGEX }
+
+  # Email Validations
   validates :email, presence: true
+  validates :email, length: { maximum: 200,
+                              minimum: 8 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -27,6 +27,20 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  def test_validation_name_length_min
+    @user.name = 'a' * 3
+    assert @user.valid?
+    @user.name = 'a' * 2
+    assert_not @user.valid?
+  end
+
+  def test_validation_name_length_max
+    @user.name = 'a' * 50
+    assert @user.valid?
+    @user.name = 'a' * 51
+    assert_not @user.valid?
+  end
+
   # Tests for User.email validation
 
   def test_validation_email_should_pass
@@ -43,5 +57,55 @@ class UserTest < ActiveSupport::TestCase
     @user.email = '   '
     assert_not @user.valid?
   end
-  # TODO: add more tests
+
+  def test_validation_email_length_min
+    @user.email = 'a' * 3 + '@a.aa'
+    assert @user.valid?
+    @user.email = 'a' * 2 + '@a.aa'
+    assert_not @user.valid?
+  end
+
+  def test_validation_email_length_max
+    @user.email = 'a' * 188 + '@example.com'
+    assert @user.valid?
+    @user.email = 'a' * 189 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  def test_validation_email_has_at
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+    check_array_is_valid(valid_addresses, true)
+  end
+
+  def test_validation_email_doesnt_have_at
+    invalid_addresses = %w[user USERfoo A_US-ERfoo.bar
+                           first.last alice+bob]
+    check_array_is_valid(invalid_addresses, false)
+  end
+
+  def text_validation_email_double_dot
+    @user.email = 'mihai@g..com'
+    assert_not @user.valid?
+  end
+
+  def test_validation_email_blank_spaces
+    invalid_addresses = ['mihai 123 blanks@mail.com', 'mihai123blanks@ma il.com']
+    check_array_is_valid(invalid_addresses, false)
+  end
+
+  # other methods
+
+  private
+
+  def check_array_is_valid(array, valid)
+    array.each do |value|
+      @user.email = value
+      if valid
+        assert @user.valid?, "#{value.inspect} should be valid"
+      else
+        assert_not @user.valid?, "#{value.inspect} should be invalid"
+      end
+    end
+  end
 end
