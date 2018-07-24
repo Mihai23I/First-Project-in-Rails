802ee861c56122b7a23927e142aa0cfd4747fe87
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-24 16:10:49 +0300

    Revert "doesn't work"
    This reverts commit ba4a411f17268d0295074aefb6733a21b22d4a93.

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   validates :password, presence: true,
-                       length: { minimum: 6 },
-                       allow_nil: true
+                       length: { minimum: 6 }
 
  def User.digest(string)
    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
