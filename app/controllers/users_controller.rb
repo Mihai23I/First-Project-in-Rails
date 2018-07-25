ae999e7d8a09729872541892e33ec62a778fba0e
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-25 11:21:35 +0300

    10.2.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -11,11 +12,32 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def logged_in_user
+   unless logged_in?
+     flash[:danger] = 'Please log in.'
+     redirect_to login_url
+   end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -16,11 +16,12 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
 
   validates :password, presence: true,
-                       length: { minimum: 6 }
+                       length: { minimum: 6 },
+                       allow_nil: true
 
  def User.digest(string)
    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                BCrypt::Engine.cost
-    BCrypt::Password.create(string, cost: cost)
+  BCrypt::Password.create(string, cost: cost)
  end
 end
