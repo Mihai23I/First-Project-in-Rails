f6bf513b27b6928969c49004d9f311c1c2b1f7ca
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-27 15:23:41 +0300

    master conflicts fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180724090952) do
+ActiveRecord::Schema.define(version: 20180727114731) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
