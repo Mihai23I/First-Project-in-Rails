99619a91c7aa47c3773a1a517a3d060754003e22
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-27 14:12:26 +0300

    10.3.1 to do exercises

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
+  end
+
+  def index
+    @users = User.all
   end
 
   def new
@@ -33,17 +38,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
+  end
+
   def logged_in_user
    unless logged_in?
+     store_location
      flash[:danger] = 'Please log in.'
      redirect_to login_url
    end
   end
-
-  private
-
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
 end
