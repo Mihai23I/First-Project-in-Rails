7585adc2ce3b4a47a6bf911496896bd5f97d2f64
Mihai23i <96mihai.ionescu@gmail.com>
2018-07-31 10:54:00 +0300

    14.2.2

--- db/seeds.rb
+++ db/seeds.rb
@@ -46,3 +46,11 @@ users.each do |user|
     user.microposts.create!(content: content)
   end
 end
+
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
